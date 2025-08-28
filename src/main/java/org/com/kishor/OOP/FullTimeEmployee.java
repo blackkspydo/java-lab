8e651d3708837837496cbd44e718324aa6d45ec2
blackkspydo <blackkspydo@gmail.com>
2025-08-28 23:37:13 +0545

    Add OOP package - Employee management system with inheritance (Employee, FullTimeEmployee, PartTimeEmployee, PayrollSystem)

---
+++ src/main/java/org/com/kishor/OOP/FullTimeEmployee.java
@@ -0,0 +1,16 @@
+package org.com.kishor.OOP;
+
+public class FullTimeEmployee extends Employee {
+
+    private double monthlySalary;
+
+    public FullTimeEmployee(int id, String name, double monthlySalary) {
+        super(id, name);
+        this.monthlySalary = monthlySalary;
+    }
+
+    @Override
+    public void calculateSalary() {
+        salary = monthlySalary; // full-time salary = monthly fixed salary
+    }
+}
---
+++ src/main/java/org/com/kishor/OOP/PartTimeEmployee.java
@@ -0,0 +1,18 @@
+package org.com.kishor.OOP;
+
+public class PartTimeEmployee extends Employee {
+
+    private int hoursWorked;
+    private double hourlyRate;
+
+    public PartTimeEmployee(int id, String name, int hoursWorked, double hourlyRate) {
+        super(id, name);
+        this.hoursWorked = hoursWorked;
+        this.hourlyRate = hourlyRate;
+    }
+
+    @Override
+    public void calculateSalary() {
+        salary = hoursWorked * hourlyRate; // part-time salary = hours * rate
+    }
+}
